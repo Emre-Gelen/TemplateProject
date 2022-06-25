8cc9704d1f48bedf2f5cefe99c9f3609dea834f5
EMRE GELEN <emregeln@gmail.com>
2022-06-25 15:55:18 +0300

    Added emailconfirmation entity.

---
+++ src/Api/Core/TemplateProject.Api.Domain/Entities/EmailConfirmation.cs
@@ -0,0 +1,10 @@
+﻿using TemplateProject.Common.Bases;
+
+namespace TemplateProject.Api.Domain.Entities
+{
+    public class EmailConfirmation : BaseEntity
+    {
+        public string NewEmailAdress{ get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}

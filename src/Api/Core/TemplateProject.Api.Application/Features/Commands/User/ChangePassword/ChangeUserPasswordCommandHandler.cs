50bd449630b68f512b4c8b794b7854308e501b18
EMRE GELEN <emregeln@gmail.com>
2022-06-25 15:56:43 +0300

    Added ChangeUserPasswordCommandHandler.

---
+++ src/Api/Core/TemplateProject.Api.Application/Features/Commands/User/ChangePassword/ChangeUserPasswordCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TemplateProject.Api.Application.Repositories;
+using TemplateProject.Common.Encryption;
+using TemplateProject.Common.Infrastructure.Exceptions;
+using TemplateProject.Common.Models.CommandModels;
+
+namespace TemplateProject.Api.Application.Features.Commands.User.ChangePassword
+{
+    public class ChangeUserPasswordCommandHandler : IRequestHandler<ChangeUserPasswordCommandModel, bool>
+    {
+        private readonly IUserWriteRepository _userWriteRepository;
+        private readonly IUserReadRepository _userReadRepository;
+
+        public ChangeUserPasswordCommandHandler(IUserWriteRepository userWriteRepository, IUserReadRepository userReadRepository, IMapper mapper)
+        {
+            _userWriteRepository = userWriteRepository;
+            _userReadRepository = userReadRepository;
+        }
+
+        public async Task<bool> Handle(ChangeUserPasswordCommandModel request, CancellationToken cancellationToken)
+        {
+            if (!request.UserId.HasValue)
+                throw new ArgumentNullException(nameof(request.UserId));
+
+            var dbUser = await _userReadRepository.GetByIdAsync(request.UserId.Value);
+
+            if (dbUser is null)
+                throw new DatabaseValidationException("User not found!");
+
+            var enchryptedPassword = PasswordEncryption.Encrypt(request.OldPassword);
+            if (dbUser.Password != enchryptedPassword)
+                throw new DatabaseValidationException("Old password is wrong!");
+
+            dbUser.Password = enchryptedPassword;
+
+            await _userWriteRepository.UpdateAsync(dbUser);
+
+            return true;
+        }
+    }
+}
---
+++ src/Common/TemplateProject.Common/Models/CommandModels/ChangeUserPasswordCommandModel.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateProject.Common.Models.CommandModels;
+public class ChangeUserPasswordCommandModel : IRequest<bool>
+{
+    public Guid? UserId { get; set; }
+    public string OldPassword{ get; set; }
+    public string NewPassword { get; set; }
+
+    public ChangeUserPasswordCommandModel(Guid? userId, string oldPassword, string newPassword)
+    {
+        UserId = userId;
+        OldPassword = oldPassword;
+        NewPassword = newPassword;
+    }
+}
